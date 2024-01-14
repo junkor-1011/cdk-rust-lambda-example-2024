0c6c57d3d8aaa52037e6c00391301ac56f4cd47d
junkor-1011 <tomoyukizxy@gmail.com>
2024-01-15 00:49:21 +0900

    feat: API Gateway + Rust Lambda Function example

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_apigateway as apigateway } from 'aws-cdk-lib';
 import { RustFunction } from 'cargo-lambda-cdk';
 import { Construct } from 'constructs';
 
@@ -11,5 +12,19 @@ export class CdkAppStack extends cdk.Stack {
       timeout: cdk.Duration.seconds(10),
       memorySize: 256,
     });
+
+    const greetFunction = new RustFunction(this, 'greet', {
+      manifestPath: 'rs-lambda/greet/Cargo.toml',
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 256,
+    });
+
+    const greetApi = new apigateway.RestApi(this, 'Greet API', {
+      restApiName: 'Greet API for test',
+    });
+
+    const greet = greetApi.root.addResource('greet');
+
+    greet.addMethod('POST', new apigateway.LambdaIntegration(greetFunction));
   }
 }
