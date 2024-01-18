32ddf035f1e3c9ed2fd24b57abb2ad953ea139c1
junkor-1011 <tomoyukizxy@gmail.com>
2024-01-19 00:14:51 +0900

    test: ignore asset hash in snapthot test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  snapshotSerializers: ['<rootDir>/test/plugins/ignore-asset-hash.ts'],
 };
+
+module.exports = config;
