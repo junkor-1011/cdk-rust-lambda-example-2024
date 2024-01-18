21a35228b30c50d5cd645ba2d99f4c1ade80b46f
junkor-1011 <tomoyukizxy@gmail.com>
2024-01-19 00:52:42 +0900

    refactor: lambda

--- rs-lambda/greet/src/main.rs
+++ rs-lambda/greet/src/main.rs
@@ -1,45 +1,59 @@
 #![allow(non_snake_case)]
 
 use aws_lambda_events::apigw::ApiGatewayProxyRequest;
-use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-
 use chrono::Utc;
-
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use serde_json;
 
 use greet::schema::{ClientErrorMessage, Message, Request, Response};
 
 async fn function_handler(event: LambdaEvent<ApiGatewayProxyRequest>) -> Result<Response, Error> {
-    let requestBodyText = event.payload.body;
+    let request_body_text = event.payload.body;
 
-    let resp: Response;
-
-    match requestBodyText {
-        Some(requestBody) => {
-            let request: Request = serde_json::from_str(&requestBody)?;
-
-            let message = Message {
-                message: "Hello, World from cargo-lambda!!!".to_string(),
-                client: format!("id={}|name={}", request.id, request.name),
-                datetime: Utc::now().to_string(),
-            };
-
-            resp = Response {
-                statusCode: 200,
-                body: serde_json::to_string(&message)?,
-            };
-        }
-        None => {
+    let request_body_text = match request_body_text {
+        Some(text) => text,
+        _ => {
             let body = ClientErrorMessage {
                 message: "No request body".to_string(),
                 datetime: Utc::now().to_string(),
             };
-            resp = Response {
+            let resp = Response {
                 statusCode: 400,
                 body: serde_json::to_string(&body)?,
             };
+            return Ok(resp);
         }
-    }
+    };
+
+    let request: Result<Request, serde_json::Error> = serde_json::from_str(&request_body_text);
+
+    let request = match request {
+        Ok(obj) => obj,
+        Err(err) => {
+            println!("[DEBUG]parse error: {:?}", err);
+            let body = ClientErrorMessage {
+                message: "validation error".to_string(),
+                datetime: Utc::now().to_string(),
+            };
+            let resp = Response {
+                statusCode: 400,
+                body: serde_json::to_string(&body)?,
+            };
+            return Ok(resp);
+        }
+    };
+
+    let resp = {
+        let message = Message {
+            message: "Hello, World from cargo-lambda!!!".to_string(),
+            client: format!("id={}|name={}", request.id, request.name),
+            datetime: Utc::now().to_string(),
+        };
+        Response {
+            statusCode: 200,
+            body: serde_json::to_string(&message)?,
+        }
+    };
     Ok(resp)
 }
 
